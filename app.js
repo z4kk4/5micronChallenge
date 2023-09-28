e8f07d7729ae3c7eaad1542e67fd8bd04e3d290e
Zakaria Marref <s0585854@htw-berlin.de>
2023-09-28 18:49:52 +0200

    server app done, postgress to go

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sensorRoute = require('./route/sensors');
+
+const app= express();
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    next();
+});
+
+app.use(sensorRoute);
+
+
+app.listen(3000)
