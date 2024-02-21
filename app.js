71304ae5a570aad801f10878ae2da1416d8153cf
Zakaria Marref <s0585854@htw-berlin.de>
2024-02-21 09:27:11 +0100

    reset for hub

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods','POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers','Content-Type');
     next();
 });
